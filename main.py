e5624ef337e92aedc498815d0e836b733ce1f869
hir0o <mk.9.q.kyu@gmail.com>
2021-09-29 16:30:20 +0900

    update

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TemplateSendMessage,
+    MessageEvent, TextMessage, TemplateSendMessage, CarouselTemplate, CarouselColumn, MessageAction, URIAction, PostbackAction, TextSendMessage, TemplateSendMessage, ConfirmTemplate
 )
 import os
  
 app = Flask(__name__)
  
 #環境変数取得
-# LINE Developersで設定されているアクセストークンとChannel Secretをを取得し、設定します。
 CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
 CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
  
@@ -25,89 +24,80 @@ handler = WebhookHandler(CHANNEL_SECRET)
 def index():
     return "ok"
  
-## 1 ##
-#Webhookからのリクエストをチェックします。
 @app.route("/callback", methods=['POST'])
 def callback():
-    # リクエストヘッダーから署名検証のための値を取得します。
     signature = request.headers['X-Line-Signature']
  
-    # リクエストボディを取得します。
+    # リクエストボディを取得
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    app.logger.info("Request body: "  + body)
  
-    # handle webhook body
-    # 署名を検証し、問題なければhandleに定義されている関数を呼び出す。
     try:
         handler.handle(body, signature)
-    # 署名検証で失敗した場合、例外を出す。
+    # 署名検証で失敗した場合、例外を出す
     except InvalidSignatureError:
         abort(400)
     # handleの処理を終えればOK
     return 'OK'
  
-## 2 ##
-###############################################
-#LINEのメッセージの取得と返信内容の設定(オウム返し)
-###############################################
- 
-#LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合に、
-#def以下の関数を実行します。
-#reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。 
-#第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
- 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
+    line_bot_api.push_message(
+        event.source.user_id,
+        TextSendMessage("こちらのレシピはいかがですか?")
+    )
+    line_bot_api.push_message(
+        event.source.user_id,
         TemplateSendMessage(
             alt_text='Carousel template',
             template=CarouselTemplate(
                 columns=[
                     CarouselColumn(
                         thumbnail_image_url='https://www.kikkoman.co.jp/homecook/search/recipe/img/00006600.jpg',
-                        title='this is menu1',
-                        text='description1',
+                        title='肉じゃが',
+                        text='追加食材: なし',
                         actions=[
-                            PostbackAction(
-                                label='postback1',
-                                display_text='postback text1',
-                                data='action=buy&itemid=1'
-                            ),
-                            MessageAction(
-                                label='message1',
-                                text='message text1'
-                            ),
                             URIAction(
-                                label='uri1',
+                                label='レシピを確認する',
                                 uri='http://example.com/1'
                             )
                         ]
                     ),
                     CarouselColumn(
                         thumbnail_image_url='https://www.kikkoman.co.jp/homecook/search/recipe/img/00006600.jpg',
-                        title='this is menu2',
-                        text='description2',
+                        title='ポトフ',
+                        text='追加食材: にんじん, 玉ねぎ',
                         actions=[
-                            PostbackAction(
-                                label='postback2',
-                                display_text='postback text2',
-                                data='action=buy&itemid=2'
-                            ),
-                            MessageAction(
-                                label='message2',
-                                text='message text2'
-                            ),
                             URIAction(
-                                label='uri2',
+                                label='レシピを確認する',
                                 uri='http://example.com/2'
                             )
                         ]
                     )
                 ]
             )
+        ) 
+    )
+    line_bot_api.push_message(
+        event.source.user_id,
+        TemplateSendMessage(
+            alt_text='Confirm template',
+            template=ConfirmTemplate(
+                text='作りたいメニューはありましたか？',
+                actions=[
+                    PostbackAction(
+                        label='はい',
+                        display_text='postback text',
+                        data='action=buy&itemid=1'
+                    ),
+                    PostbackAction(
+                        label='いいえ',
+                        display_text='postback text',
+                        data='action=buy&itemid=1'
+                    ),
+                ]
+            )
         )
-        # TextSendMessage(text=event.message.text)) 
     )
 
 # ポート番号の設定
