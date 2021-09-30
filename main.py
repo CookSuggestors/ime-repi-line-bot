702f07cc2076fa661c438598ab849145b7cf2acc
hir0o <mk.9.q.kyu@gmail.com>
2021-09-30 16:38:14 +0900

    リファクタリング

--- main.py
+++ main.py
@@ -41,11 +41,10 @@ def index():
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
- 
     # リクエストボディを取得
     body = request.get_data(as_text=True)
     app.logger.info("Request body: "  + body)
- 
+
     try:
         handler.handle(body, signature)
     # 署名検証で失敗した場合、例外を出す
@@ -54,12 +53,98 @@ def callback():
     # handleの処理を終えればOK
     return 'OK'
  
+# メッセージが送信された時の処理
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    # TODO: 材料が送信されているかどうか
     user_id = event.source.user_id
     sendMessage("こちらのレシピはいかがですか？", user_id)
-    # レシピデータを追加
-    recipeData[user_id] = {"index": 0, "recipe": [
+    # ユーザーデータを作成
+    recipeData[user_id] = {"index": 0, "recipe": getRecipe()}
+    sendCarousel(getDisplayCarousel(recipeData, user_id), user_id)
+    sendConfirm(user_id)
+
+# ユーザが操作した後の処理
+@handler.add(PostbackEvent)
+def handle_event(event):
+    user_id = event.source.user_id
+    if event.postback.data == "yes":
+       del recipeData[user_id]
+       sendMessage("調理頑張ってください！", user_id)
+    else:
+        # レシピが提案できるかどうか
+        if(recipeData[user_id]["index"] + 2 <= len(recipeData[user_id]["recipe"])):
+            sendMessage("こちらのレシピはいかがでしょうか?", user_id)
+            sendCarousel(getDisplayCarousel(recipeData, user_id), user_id)
+            sendConfirm(user_id)
+        else:
+            del recipeData[user_id]
+            sendMessage("ごめんなさい！これ以上レシピを提案できません(>_<)", user_id)
+
+# メッセージの送信
+def sendMessage(message, user_id):
+    line_bot_api.push_message(
+        user_id,
+        TextSendMessage(message)
+    )
+
+# 確認メッセージの送信
+def sendConfirm(user_id):
+    line_bot_api.push_message(
+        user_id,
+        TemplateSendMessage(
+            alt_text='Confirm template',
+            template=ConfirmTemplate(
+                text='作りたいメニューはありましたか？',
+                actions=[
+                    PostbackAction(
+                        label='はい',
+                        display_text='はい',
+                        data='yes'
+                    ),
+                    PostbackAction(
+                        label='いいえ',
+                        display_text='いいえ',
+                        data='no'
+                    ),
+                ]
+            )
+        )
+    )
+
+# カルーセルの送信
+def sendCarousel(col, user_id):
+    line_bot_api.push_message(
+        user_id,
+        TemplateSendMessage(
+            alt_text = 'Carousel template',
+            template = CarouselTemplate(
+                columns = col
+            )
+        ) 
+    )
+
+# 表示するカルーセルの取得
+def getDisplayCarousel(recipeData, user_id):
+    index = recipeData[user_id]["index"]
+    recipeData[user_id]["index"] += 2
+    caroucelCol = []
+    for recipe in recipeData[user_id]["recipe"][index:index + 2]:
+        caroucelCol.append(CarouselColumn(
+            thumbnail_image_url = recipe["image_url"],
+            title = recipe["title"],
+            text = "，".join(recipe["material"]),
+            actions=[
+                URIAction(
+                    label='レシピを確認する',
+                    uri = recipe["url"]
+                )
+            ]
+        ))
+    return caroucelCol
+
+def getRecipe():
+    return [
         {
             "title": "肉じゃが",
             "image_url": "https://www.kikkoman.co.jp/homecook/search/recipe/img/00006600.jpg",
@@ -84,116 +169,7 @@ def handle_message(event):
             "material": ["にんじん", "タマネギ"],
             "url": "https://www.kikkoman.co.jp/homecook/search/recipe/00006600/index.html"
         }
-    ]}
-    caroucelCol = []
-    for recipe in recipeData[user_id]["recipe"][0:2]:
-        caroucelCol.append(CarouselColumn(
-            thumbnail_image_url = recipe["image_url"],
-            title = recipe["title"],
-            text = "，".join(recipe["material"]),
-            actions=[
-                URIAction(
-                    label='レシピを確認する',
-                    uri = recipe["url"]
-                )
-            ]
-        ))
-    line_bot_api.push_message(
-        user_id,
-        TemplateSendMessage(
-            alt_text = 'Carousel template',
-            template = CarouselTemplate(
-                columns = caroucelCol
-            )
-        ) 
-    )
-    line_bot_api.push_message(
-        user_id,
-        TemplateSendMessage(
-            alt_text='Confirm template',
-            template=ConfirmTemplate(
-                text='作りたいメニューはありましたか？',
-                actions=[
-                    PostbackAction(
-                        label='はい',
-                        display_text='はい',
-                        data='yes'
-                    ),
-                    PostbackAction(
-                        label='いいえ',
-                        display_text='いいえ',
-                        data='no'
-                    ),
-                ]
-            )
-        )
-    )
-
-@handler.add(PostbackEvent)
-def handle_event(event):
-    user_id = event.source.user_id
-    if event.postback.data == "yes":
-       del recipeData[user_id]
-       sendMessage("調理頑張ってください！", user_id)
-    else:
-        # レシピが提案できるかどうか
-        if(recipeData[user_id]["index"] + 2 < len(recipeData[user_id]["recipe"])):
-            sendMessage("こちらのレシピはいかがでしょうか?", user_id)
-            recipeData[user_id]["index"] += 2
-            index = recipeData[user_id]["index"]
-            caroucelCol = []
-            for recipe in recipeData[user_id]["recipe"][index:index + 2]:
-                caroucelCol.append(CarouselColumn(
-                    thumbnail_image_url = recipe["image_url"],
-                    title = recipe["title"],
-                    text = "，".join(recipe["material"]),
-                    actions=[
-                        URIAction(
-                            label='レシピを確認する',
-                            uri = recipe["url"]
-                        )
-                    ]
-                ))
-            line_bot_api.push_message(
-                user_id,
-                TemplateSendMessage(
-                    alt_text = 'Carousel template',
-                    template = CarouselTemplate(
-                        columns = caroucelCol
-                    )
-                ) 
-            )
-            line_bot_api.push_message(
-                user_id,
-                TemplateSendMessage(
-                    alt_text='Confirm template',
-                    template=ConfirmTemplate(
-                        text='作りたいメニューはありましたか？',
-                        actions=[
-                            PostbackAction(
-                                label='はい',
-                                display_text='はい',
-                                data='yes'
-                            ),
-                            PostbackAction(
-                                label='いいえ',
-                                display_text='いいえ',
-                                data='no'
-                            ),
-                        ]
-                    )
-                )
-            )
-        else:
-            del recipeData[user_id]
-            sendMessage("ごめんなさい！これ以上レシピを提案できません(>_<)", user_id)
-
-def sendMessage(message, user_id):
-    line_bot_api.push_message(
-        user_id,
-        TextSendMessage(message)
-    )
-
+    ]
 # ポート番号の設定
 if __name__ == "__main__":
 #    app.run()
